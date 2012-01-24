0e1225c2653fa16df3edbf1812f8d51332024b33
helino <erik.helin@gmail.com>
2012-01-24 17:44:40 +0100

    kmain now displays info about loaded modules
    Also updated all memory adjustments to use the PHYSICAL_TO_VIRTUAL macro
    instead of manually ajdusting memory locations

--- src/kmain.c
+++ src/kmain.c
@@ -29,14 +29,15 @@ void display_tick()
 
 multiboot_info_t *remap_multiboot_info(uint32_t mbaddr)
 {
-    multiboot_info_t *mbinfo = (multiboot_info_t *) (mbaddr + KERNEL_BASE_ADDR);
+    multiboot_info_t *mbinfo = (multiboot_info_t *) PHYSICAL_TO_VIRTUAL(mbaddr);
 
-    mbinfo->mmap_addr += KERNEL_BASE_ADDR;
+    mbinfo->mmap_addr = PHYSICAL_TO_VIRTUAL(mbinfo->mmap_addr);
+    mbinfo->mods_addr = PHYSICAL_TO_VIRTUAL(mbinfo->mods_addr);
 
     return mbinfo;
 }
 
-void display_memory_info(multiboot_info_t *mbinfo, kernel_meminfo_t *mem)
+void display_memory_map(multiboot_info_t *mbinfo)
 {
     /* From the GRUB multiboot manual section 3.3 boot information format
      * If flags[0] is set, then the fields mem_lower and mem_upper can be 
@@ -66,11 +67,36 @@ void display_memory_info(multiboot_info_t *mbinfo, kernel_meminfo_t *mem)
                 (((uint32_t) entry) + entry->size + sizeof(entry->size));
         }
     }
+    printf("\n");
+}
 
+void display_kernel_mem_info(kernel_meminfo_t *mem) {
     printf("kernel physical start: %X\n", mem->kernel_physical_start);
     printf("kernel physical end: %X\n", mem->kernel_physical_end);
     printf("kernel virtual start: %X\n", mem->kernel_virtual_start);
     printf("kernel virtual end: %X\n", mem->kernel_virtual_end);
+    printf("\n");
+}
+
+void display_module_info(multiboot_info_t *mbinfo)
+{
+    uint32_t i;
+    char *name;
+    if (mbinfo->flags & 0x00000008) {
+        printf("Number of modules: %u\n", mbinfo->mods_count);
+        multiboot_module_t *module = (multiboot_module_t *) mbinfo->mods_addr;
+        for (i = 0; i < mbinfo->mods_count; ++i, ++module) {
+            if (module->cmdline == 0) {
+                printf("module: no cmdline found\n");
+            } else {
+                name = (char *) PHYSICAL_TO_VIRTUAL(module->cmdline);
+                printf("module %s\n", name);
+            }
+            printf("\tstart: %X\n", module->mod_start);
+            printf("\tend: %X\n", module->mod_end);
+        }
+
+    }
 }
 
 int kmain(uint32_t mbaddr, uint32_t magic_number, kernel_meminfo_t mem,
@@ -88,7 +114,9 @@ int kmain(uint32_t mbaddr, uint32_t magic_number, kernel_meminfo_t mem,
     kinit(&mem, boot_page_directory);
 
     printf("Welcome to aenix!\n");
-    display_memory_info(mbinfo, &mem);
+    display_memory_map(mbinfo);
+    display_kernel_mem_info(&mem);
+    display_module_info(mbinfo);
 
     /*pit_set_callback(1, &display_tick); */
 
