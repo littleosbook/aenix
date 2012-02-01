4f543eeda0084521a263dceccc9ed1608d06d3e8
helino <erik.helin@gmail.com>
2012-02-01 10:41:45 +0100

    loader.s now includes constants.h for #defines
    Also updated Makefile to reflect this

--- src/loader.s
+++ src/loader.s
@@ -2,6 +2,8 @@
 ; the main purpose of this file it to set up symbols for the bootloader
 ; and jump to the kmain function in the kernel
 
+%include "constants.inc"
+
 global loader                           ; the entry point for the linker
 
 extern kmain                            ; kmain is defined in kmain.c
@@ -19,21 +21,9 @@ MAGIC       equ 0x1BADB002              ; magic number for bootloader to
                                         ; find the header
 CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum required
 
-; because mistyping is easy
-FOUR_KB         equ 0x00001000
-FOUR_MB         equ 0x00400000
-
-; paging for the kernel
-KERNEL_VIRTUAL_BASE equ 0xC0000000                  ; we start at 3GB
-KERNEL_PDT_IDX      equ KERNEL_VIRTUAL_BASE >> 22   ; index = highest 10 bits
-
 ; we set Page write-through, Writable, Present
 KERNEL_PT_CFG       equ 00000000000000000000000000001011b
 KERNEL_PDT_ID_MAP   equ 00000000000000000000000010001011b
-
-; stack management
-; the stack grows from the end of the page towards lower address
-KERNEL_STACK_SIZE equ 0x400
 
 ; the page directory used to boot the kernel into the higher half
 section .data
@@ -64,20 +54,20 @@ align 4
 
 ; the entry point, called by GRUB
 loader:
-    mov ecx, (grub_magic_number - KERNEL_VIRTUAL_BASE)
+    mov ecx, (grub_magic_number - KERNEL_START_VADDR)
     mov [ecx], eax
-    mov ecx, (grub_multiboot_info - KERNEL_VIRTUAL_BASE)
+    mov ecx, (grub_multiboot_info - KERNEL_START_VADDR)
     mov [ecx], ebx
 
 set_up_kernel_pdt:
     ; set up kernel_pdt to point to kernel_pt
-    mov ecx, (kernel_pdt - KERNEL_VIRTUAL_BASE + KERNEL_PDT_IDX*4)
-    mov edx, (kernel_pt - KERNEL_VIRTUAL_BASE)
+    mov ecx, (kernel_pdt - KERNEL_START_VADDR + KERNEL_PDT_IDX*4)
+    mov edx, (kernel_pt - KERNEL_START_VADDR)
     or  edx, KERNEL_PT_CFG
     mov [ecx], edx
 
 set_up_kernel_pt:
-    mov eax, (kernel_pt - KERNEL_VIRTUAL_BASE)
+    mov eax, (kernel_pt - KERNEL_START_VADDR)
     mov ecx, KERNEL_PT_CFG
 .loop:
     mov [eax], ecx
@@ -87,7 +77,7 @@ set_up_kernel_pt:
     jle .loop
 
 enable_paging:
-    mov ecx, (kernel_pdt - KERNEL_VIRTUAL_BASE)
+    mov ecx, (kernel_pdt - KERNEL_START_VADDR)
     and ecx, 0xFFFFF000     ; we only care about the upper 20 bits
     or  ecx, 0x08           ; PWT, enable page write through?
     mov cr3, ecx            ; load pdt
@@ -122,4 +112,3 @@ enter_kmain:
     call kmain                          ; call the main function of the kernel
 hang:
     jmp hang                            ; loop forever
-
